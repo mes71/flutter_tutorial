560e43216ff0eecd096bed0cdb266bcff7a9a692
Mr.Mojtaba <Mr.Mojtaba47@gmail.com>
2023-09-06 09:51:55 +0330

    feat : add app state to local value as pair

--- build_apps_with_flutter/lib/main.dart
+++ build_apps_with_flutter/lib/main.dart
@@ -38,12 +38,12 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-
+    var pair = appState.current;
     return  Scaffold(
       body: Column(
         children: [
           Text('A random AWESOME idea:'),
-          Text(appState.current.asLowerCase),
+          Text(pair.asLowerCase),
 
           // ↓ Add this.
           ElevatedButton(
